23f1895bb2a72db6d25c4a83deabcf864bd49d49
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-04-30 22:13:09 +0600

    arrag added

---
+++ Chapter-2/array.js
@@ -0,0 +1,28 @@
+let ninjas = ['Mahmudul', 'islam', 'robince'];
+
+console.log(ninjas);
+console.log(ninjas[1]);
+
+let ageArray = [20, 25,30, 35];
+console.log(ageArray);
+console.log(ageArray[2]);
+
+let random = ['time', 'year', 6.23, 2022];
+console.log(random);
+console.log('length: ', random.length);
+
+
+let joinNinjas = ninjas.join('*');
+console.log(joinNinjas);
+
+let reverseNinjas = ninjas.reverse();
+console.log(reverseNinjas);
+reverseNinjas = ninjas.reverse();
+
+let indexNinjas = ninjas.indexOf('islam');
+console.log('index: ', indexNinjas);
+
+let concatNinjas = ninjas.concat(['iot', 'engineer']);
+console.log(concatNinjas);
+
+
